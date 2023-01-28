3c8b129014869b4c7a105c15e82af08292fca4b8
tilkeii <florian@gouarin.info>
2023-01-28 20:01:24 +0100

    init

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eu
+
+echo "Updating, upgrading and cleaning distribution"
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y autoremove
+
+if ! command -v git >/dev/null; then
+	echo "Installing git"
+	sudo apt -y install git
+fi
+
+if ! command -v zsh >/dev/null; then
+	echo "Installing ZSH"
+	sudo apt -y install zsh
+
+	echo "Setting ZSH as default shell"
+	sudo chsh -s $(which zsh)
+fi
+
+if [ ! -f ~/antigen.zsh ]; then
+	echo "Installing antigen"
+	curl -L git.io/antigen > $HOME/antigen.zsh
+fi
+
+echo "Copying zsh configuration"
+cp .zshrc $HOME/.zshrc
+
+echo "Copying powerlevel10k configuration"
+cp .p10k.zsh $HOME/.p10k.zsh
